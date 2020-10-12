4425dbe7a9355b8579f7fa0de5504b3c13f80d27
Szymon Burak <sajmon0031@gmail.com>
2020-10-12 20:51:51 +0200

    Adjustments

--- src/components/Lighthouse.js
+++ src/components/Lighthouse.js
@@ -18,12 +18,12 @@ const Lighthouse = () => {
     return (<section className="lighthouse">
         <h2>Maksymalna wydajność stron</h2>
         <h3>Doskonały performance, pełna responsywność i optymalizacja pod SEO</h3>
-        <img src={require("../../static/img/medal.png")} alt="medal" className="medal only-1250" />
         <div className="row-container">
             <div className="first-row">
                 <img src={require("../../static/img/lighthouse-logo.png")} alt="lighthouse" className="lighthouse-img" />
                 <h4>Lighthouse</h4>
                 <p>Do pomiaru poziomów wydajności witryny może posłużyć narzędzie Lighthouse. Prosty w obsłudze dodatek pokaże nam jakie wartości przyjmuje dana strona, np. w zakresie pozycjonowania SEO lub wydajności strony.</p>
+                <img src={require("../../static/img/medal.png")} alt="medal" className="medal only-1250" />
             </div>
             <div className="second-row">
                 <Img fluid={data.pomiary.childImageSharp.fluid} alt="pomiary" />
@@ -31,8 +31,8 @@ const Lighthouse = () => {
                 <p>Jak widać na załączonym obrazku, pomiary na jednym ze zrealizowanych przez nas projektów osiągają prawie maksymalną wartość przy każdym elemencie.</p>
             </div>
             <div className="third-row">
-                <h3>Nasze projekty to <span className="red">w pełni profesjonalne realizacje.</span></h3>
-                <h4>Zadbaj o swój biznes razem z nami</h4>
+                <h3>Nasze projekty to <span className="red bold">w pełni profesjonalne realizacje</span>.</h3>
+                <h4>Zadbaj o swój biznes razem z nami.</h4>
             </div>
         </div>
     </section>);
